d4c05416ab213b9f9bac9ba90fcb687c503a6def
tss-smart <78737629+tss-smart@users.noreply.github.com>
2021-02-08 21:06:49 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    AGE += 1
+})
+let timer = 0
+let AGE = 0
+let Remind_time_period = 0
+basic.forever(function () {
+    basic.pause(1000)
+    timer += timer
+    if (AGE == 1) {
+    	
+    }
+})
